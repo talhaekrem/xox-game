fcdacdb51d7d9ee3e2517c6dce85023add537ab5
Talha <talha.ekrem.99@gmail.com>
2022-08-25 01:31:22 +0300

    github-link-for-publish

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import { GameProvider } from "./context/GameContext";
 function App() {
   return (
     <div className="app">
-      <h1 className="pb-3 fw-100">Tic-tac-toe</h1>
+      <h1 className="fw-100">Tic-tac-toe</h1>
+      <a
+        rel="noreferrer"
+        href="https://github.com/talhaekrem"
+        target="_blank"
+        className="githubLink"
+      >
+        <pre>My Github Profile</pre>
+      </a>
       <GameProvider>
         <GameBox />
         <PlayersTurnArea />
